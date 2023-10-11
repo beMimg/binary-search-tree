609f21b5f0cd6ca40166933ace134f6edb9c40da
bernyig <bernardommguerreiro@gmail.com>
2023-10-11 23:40:27 +0200

    insert functional

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import { Tree, prettyPrint } from './tree.js';
 
-let test = [0, 1, 2, 3, 4, 1, 2, 3, 5, 6, 7];
+let test = [2];
 const tree = new Tree(test);
 prettyPrint(tree.root);
 console.log(tree);
+tree.insert(1);
+tree.insert(3);
+tree.insert(4);
+tree.insert(7);
+tree.insert(5);
--- tree.js
+++ tree.js
@@ -16,12 +16,40 @@ export class Tree {
       return null;
     }
 
-    let mid = Math.floor((start + end) / 2);
-    let root = new Node(mid);
+    let mid = parseInt((start + end) / 2);
+    let root = new Node(this.arr[mid]);
 
     root.left = this.buildTree(arr, start, mid - 1);
     root.right = this.buildTree(arr, mid + 1, end);
 
+    return root;
+  }
+
+  insert(value, root = this.root) {
+    if (root == null) {
+      root = new Node(value);
+      return;
+    }
+
+    let current = this.root;
+    let prev = null;
+    let newNode = new Node(value);
+
+    while (current !== null) {
+      if (value > current.data) {
+        prev = current;
+        current = current.right;
+      } else if (value < current.data) {
+        prev = current;
+        current = current.left;
+      }
+    }
+    if (value > prev.data) {
+      prev.right = newNode;
+    } else if (value < prev.data) {
+      prev.left = newNode;
+    }
+    prettyPrint(root);
     return root;
   }
 }
